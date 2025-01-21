d4c16a825a4d91ae110448d53c30064dd7c2ff6e
changan-memories <qinyuxiangzzu@outlook.com>
2025-01-21 17:56:10 +0800

    update

--- Version_C/Queue_Version_Link/Queue_Version_Link/Queue.c
+++ Version_C/Queue_Version_Link/Queue_Version_Link/Queue.c
@@ -1,2 +1,18 @@
 #include "Queue.h"
 
+//初始化队列
+void QueuInit(Queue* q);
+// 队尾入队列
+void QueuePush(Queue* q, QDataType ele);
+// 队头出队列
+void QueuePop(Queue* q);
+// 获取队列头部元素
+QDataType QueueFront(Queue* q);
+// 获取队列尾部元素
+QDataType QueueBack(Queue* q);
+//获取队列中有效元素个数
+int QueueSize(Queue* q);
+//检测队列是否为空，如果为空返回非零结果，如果非空返回0
+int QueueEmpty(Queue* q);
+// 销毁队列
+void QueueDestroy(Queue* q);
--- Version_C/Queue_Version_Link/Queue_Version_Link/Queue.h
+++ Version_C/Queue_Version_Link/Queue_Version_Link/Queue.h
@@ -7,7 +7,26 @@
 //队列，先进先出，数组的话在头部出数据不方便，因此用链表来实现，单链表
 typedef int QDataType;
 
-struct QuqueNode {		//链式队列，用单链表实现
+typedef struct QuqueNode {		//链式队列，用单链表实现
 	struct QueueNode* next;
 	QDataType data;
-};
+}Queue;
+
+//初始化队列
+void QueueInit(Queue* q);
+// 队尾入队列
+void QueuePush(Queue* q, QDataType ele);
+// 队头出队列
+void QueuePop(Queue* q);
+// 获取队列头部元素
+QDataType QueueFront(Queue* q);
+// 获取队列尾部元素
+QDataType QueueBack(Queue* q);
+//获取队列中有效元素个数
+int QueueSize(Queue* q);
+//检测队列是否为空，如果为空返回非零结果，如果非空返回0
+int QueueEmpty(Queue* q);
+// 销毁队列
+void QueueDestroy(Queue* q);
+
+
