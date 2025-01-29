1ce8c322256e637abe01ef158aa62030784d7d7f
changan-memories <qinyuxiangzzu@outlook.com>
2025-01-29 21:03:18 +0800

    update Heap

--- Version_C/Heap_classic_BinTree/Heap_classic_BinTree/Heap.c
+++ Version_C/Heap_classic_BinTree/Heap_classic_BinTree/Heap.c
@@ -1,2 +1,58 @@
 #include "Heap.h"
 
+void HeapInit(Heap* pheap) {
+	assert(pheap);
+	
+	pheap->base = (HeapDataType*)malloc(sizeof(HeapDataType) * 4);
+	if (pheap->base == NULL) {
+		perror("malloc failed\n");
+		return;
+	}
+	pheap->size = 0;
+	pheap->capacity = 4;
+}
+
+void Swap(HeapDataType* child, HeapDataType* parent) {
+	HeapDataType* temp = *child;
+	*child = *parent;
+	*parent = temp;
+}
+//向上调整
+void AdjustUp(HeapDataType* arr, int child) {
+	assert(arr);
+	int parent = (child - 1) / 2;
+
+	while (parent >= 0) {
+		if (arr[child] > arr[parent]) {
+			Swap(&arr[child], &arr[parent]);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+void HeapPush(Heap* pheap, HeapDataType data) {
+	assert(pheap);	//空堆也可以push，但需要保证结构体存在
+
+	//插入，检查是否需要扩容
+	if (pheap->size == pheap->capacity) {
+		HeapDataType* newSpace = (HeapDataType*)realloc(pheap->base, sizeof(HeapDataType) * pheap->capacity * 2);
+		if (newSpace == NULL) {
+			perror("realloc fialed\n");
+			return;
+		}
+		pheap->base = newSpace;
+
+		pheap->base[pheap->size] = data;
+		pheap->size++;
+		pheap->capacity *= 2;
+
+		//插入需要向上调整
+		AdjustUp(pheap->base, pheap->size - 1);	//size++ 后，size-1 是新插入元素的位置
+	}
+
+
+}
--- Version_C/Heap_classic_BinTree/Heap_classic_BinTree/Heap.h
+++ Version_C/Heap_classic_BinTree/Heap_classic_BinTree/Heap.h
@@ -7,7 +7,24 @@
 //在顺序存储的二叉树上加一些限定条件，定义为堆
 
 //堆中某个节点的值总是不大于或不小于其父节点的值
-//堆总是一棵完全二叉树
+//堆总是一棵    完全二叉树
 
 //小根堆   树中所有父亲都小于或等于孩子
 //大根堆   树中所有父亲都大于或等于孩子
+//堆可以用来排序，但堆并非是有序的  
+
+//堆的向上调整
+
+//实现大堆
+typedef int HeapDataType;
+typedef struct HeapNode {
+	HeapDataType* base;
+	int size;
+	int capacity;
+}Heap;
+
+void HeapInit(Heap* pheap);
+
+void HeapPush(Heap* pheap, HeapDataType data);
+
+
