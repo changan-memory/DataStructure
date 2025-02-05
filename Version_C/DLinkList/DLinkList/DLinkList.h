be1483cf3a0da60912070967d96438f3245c68e2
changan-memories <qinyuxiangzzu@outlook.com>
2025-02-05 21:17:38 +0800

    update

--- Version_C/DLinkList/DLinkList/DLinkList.h
+++ Version_C/DLinkList/DLinkList/DLinkList.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 //双向循环链表
 typedef int LTDataType;
@@ -15,9 +15,11 @@ typedef struct ListNode {
 
 //申请结点
 ListNode* BuyListNode(LTDataType data);
-//初始化双向循环链表
+
+//初始化与销毁双向循环链表
 ListNode* ListInit();
 void ListDestroy(ListNode* phead);
+
 //打印链表
 void ListPrint(ListNode* phead);
 
@@ -29,10 +31,10 @@ void ListPopBack(ListNode* phead);
 void ListPushFront(ListNode* phead, LTDataType data);
 void ListPopFront(ListNode* phead);
 
-//查找
+//查找与判空
 ListNode* ListFind(ListNode* phead, LTDataType data);
-//判断是否为空
 bool ListEmpty(ListNode* phead);
+
 //在pos前面进行插入
 void ListInsert(ListNode* pos, LTDataType data);
 //删除pos位置的结点
